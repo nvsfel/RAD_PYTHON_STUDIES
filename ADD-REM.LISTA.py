917bbd16aeb7f01ff62b4ba7e6fb7d42150d97b4
Felipe Bezerra <nas.felipe73@gmail.com>
2026-03-25 20:04:30 -0300

    Add files via upload

---
+++ ADD-REM.LISTA.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox
+
+dados = {}
+
+def adicionar():
+    nome = entry_nome.get()
+    idade = entry_idade.get()
+    if nome == "" or idade =="":
+        messagebox.showwarning("Aviso","Preencha todos os campos!")
+        return
+
+    dados[nome] = idade
+    atualizar_lista()
+    entry_nome.delete(0,tk.END)
+    entry_idade.delete(0,tk.END)
+
+def atualizar_lista():
+    lista.delete(0, tk.END)
+    for nome, idade in dados.items():
+        lista.insert(tk.END, f"{nome} - {idade} anos")
+
+def remover():
+    selecionado = lista.curselection()
+    if not selecionado:
+        messagebox.showwarning("Aviso", "Selecione alguém para remover!")
+        return
+
+    texto = lista.get(selecionado)
+    nome = texto.split(" - ")[0]
+    del dados[nome]
+    atualizar_lista()
+
+janela = tk.Tk()
+janela.title("Cadastro com Dicionário")
+
+tk.Label(janela, text="Nome:").pack()
+entry_nome = tk.Entry(janela)
+entry_nome.pack()
+
+tk.Label(janela, text="Idade:").pack()
+entry_idade = tk.Entry(janela)
+entry_idade.pack()
+
+tk.Button(janela, text="Adicionar", command = adicionar).pack(pady=5)
+tk.Button(janela, text="Remover", command= remover).pack(pady=5)
+lista=tk.Listbox(janela, width=40)
+lista.pack(pady=10)
+
+janela.mainloop()
