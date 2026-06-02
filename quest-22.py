40602205be41a8035da7e1478be6f9d7f8f5b135
Felipe Bezerra <nas.felipe73@gmail.com>
2026-06-02 17:52:00 -0300

    Adição de output com dados coletados
    fix: Atualização da velocidade média na função calcular_vm

--- quest-22.py
+++ quest-22.py
@@ -1,7 +1,7 @@
 def calcular_vm(distancia, tempo):
     #nome nos parâmetros não interfere na variável. pode ser qualquer nome.
     try:
-        velocidade_media = (distancia/100)/(tempo/60)
+        velocidade_media = (distancia/1000)/(tempo/60)
 
         if velocidade_media < 8:
             status = "Lento!"
@@ -12,29 +12,30 @@ def calcular_vm(distancia, tempo):
         else:
             status = "Elite."
             
-            return velocidade_media, status #retornar ambos valores
+        return velocidade_media, status #retornar ambos valores
         
     except ValueError:
         print("Insira valores válidos!")
-        return
+        return 0.0, "Erro nos dados!"
 
 atletas = {}
 
 while True:
 
     chave = input("Digite qualquer tecla para iniciar, ou 'sair' para fechar.\n").strip()
-    if chave.lower=='sair':
+    if chave.lower()=='sair':
         break
     
     
     raias = int(input("Quantos atletas serão avaliados?"))
     
     for corredores in range(raias):
-        nome = input("Nome do atleta:").title()
-        dist = float(input("Digite, em metros, a distância percorrida"))
-        tempo = float(input("Digite, em minutos, o tempo percorrido"))
+        nome = input("Nome do atleta:\n").title()
+        dist = float(input("Digite, em metros, a distância percorrida:\n"))
+        tempo = float(input("Digite, em minutos, o tempo percorrido:\n"))
         
         vm_atleta, status = calcular_vm(dist, tempo)
+
         
         atletas[nome] = {
             "distancia" : dist,
@@ -42,8 +43,10 @@ while True:
             "velocidade" : vm_atleta,
             "status" : status
             }
+    for nome, dados in atletas.items():
+        print("|=======================|")
+        print(f"Atleta:{nome}\nDistância Percorrida:{dados['distancia']:.2f}\nTempo:{dados['tempo']:.2f}\nRitmo:{dados['velocidade']:.2f}km/h\nStatus:{dados['status']}")
+        print("|=======================|")   
 
-   
-
-    print(atletas)
+    
     
